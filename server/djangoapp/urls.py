9f37ff94d9d916061f9bfe7ba746b81ff13491f0
Florence Y <xxxflory26xxx@gmail.com>
2022-04-29 04:04:42 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -26,18 +26,4 @@ urlpatterns = [
 
     # path for add a review view
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-'''from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from . import views
-
-app_name = 'onlinecourse'
-urlpatterns = [
-    # Add path here
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
- + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-'''
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
