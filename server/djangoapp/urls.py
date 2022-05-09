cd14c201091b58edc3fa48e875f488bcbca72d17
Florence Y <xxxflory26xxx@gmail.com>
2022-05-09 01:58:02 +0100

    update

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,10 +15,11 @@ urlpatterns = [
     # path for contact us view
     path(route='contact/', view=views.contact, name='contact_us'),
     # path for registration
-
+    path('registration/', views.registration_request, name='registration'),
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
